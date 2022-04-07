338b1de8a81734def7600aac3aefad0b4a7cb810
Yara Schattschneider <61124827+yarasc@users.noreply.github.com>
2022-04-07 13:48:10 +0200

    tesing + dif timewindow

--- dvs_test.py
+++ dvs_test.py
@@ -1,23 +1,27 @@
+import time
+
 import matplotlib.pyplot as plt
+import numpy as np
+import snntorch as snn
 import tonic
 import tonic.transforms as transforms
 import torch
 import torch.nn as nn
 import torchvision
-from torch.utils.data import DataLoader
-from tonic import DiskCachedDataset
-
-import snntorch as snn
 from snntorch import functional as SF
 from snntorch import surrogate
 from snntorch import utils
+from tonic import DiskCachedDataset
+from torch.utils.data import DataLoader
 
+np.set_printoptions(threshold=100000)
+import snntorch.spikeplot as splt
+from IPython.display import HTML
 
 """
 Feedforward NN with LIF
 based on Tutorial 7 SNNTorch
 """
-
 
 """
 #########################################
@@ -33,7 +37,7 @@ sensor_size = tonic.datasets.DVSGesture.sensor_size
 # time_window
 frame_transform = transforms.Compose([transforms.Denoise(filter_time=10000),
                                       transforms.ToFrame(sensor_size=sensor_size,
-                                                         time_window=1000)
+                                                         time_window=3000)
                                       ])
 
 trainset = tonic.datasets.DVSGesture(save_to='./data', transform=frame_transform, train=True)
@@ -46,27 +50,36 @@ TRAINING OUR NETWORK USING FRAMES CREATED FROM EVENTS
 """
 
 transform = tonic.transforms.Compose([torch.from_numpy,
-                                      torchvision.transforms.RandomRotation([-10,10])])
+                                      torchvision.transforms.RandomRotation([-10, 10])])
 
-#cached_trainset = DiskCachedDataset(trainset, transform=transform, cache_path='./cache/dvs/train')
+cached_trainset = DiskCachedDataset(trainset, transform=transform, cache_path='./cache/dvs/train')
+cached_testset = DiskCachedDataset(testset, cache_path='./cache/dvs/test')
 
 # no augmentations for the testset
-#cached_testset = DiskCachedDataset(testset, cache_path='./cache/dvs/test')
+# cached_testset = DiskCachedDataset(testset, cache_path='./cache/dvs/test')
 
 
 batch_size = 16
 trainloader = DataLoader(trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
-#testloader = DataLoader(cached_testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
+testloader = DataLoader(testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors())
 
+n = 125
+# index into a single sample and sum the on/off channels
+# a = (trainloader.dataset[n][0][:, 0] + trainloader.dataset[n][0][:, 1])
+# #  Plot
+# fig, ax = plt.subplots()
+# anim = splt.animator(a, fig, ax, interval=10)
+# HTML(anim.to_html5_video())
+# anim.save('dvsgesture_animator.mp4', writer = 'ffmpeg', fps=50)
 """
 DEFINING OUR NETWORK
 """
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 # network parameters
-num_inputs = 28*28
-num_hidden = 128
-num_outputs = 10
+num_inputs = 128 * 128 * 2  # width*height*channels (on-spikes for luminance increasing; off-spikes for luminance decreasing)
+num_hidden = 128 * 128 * 2
+num_outputs = 11
 num_steps = 1
 
 # spiking neuron parameters
@@ -75,11 +88,11 @@ grad = surrogate.fast_sigmoid()
 
 #  Initialize Network
 net = nn.Sequential(
-                        nn.Flatten(),
-                        nn.Linear(32*32*32, 11),
-                        snn.Leaky(beta=beta, spike_grad=grad, init_hidden=True, output=True),
+    nn.Flatten(),
+    nn.Linear(num_inputs, num_outputs),
+    snn.Leaky(beta=beta, spike_grad=grad, init_hidden=True, output=True),
 
-                    ).to(device)
+).to(device)
 
 
 # this time, we won't return membrane as we don't need it
@@ -102,21 +115,25 @@ optimizer = torch.optim.Adam(net.parameters(), lr=2e-2, betas=(0.9, 0.999))
 loss_fn = SF.ce_rate_loss()
 
 num_epochs = 1
-num_iters = 50
+num_iters = 100
 
 loss_hist = []
 acc_hist = []
+test_loss_hist = []
+test_acc_hist = []
 
 # training loop
+t0 = time.time()
 for epoch in range(num_epochs):
     for i, (data, targets) in enumerate(trainloader, 0):
-        print("test")
         data = data.to(device)
         targets = targets.to(device)
+        # data = data.to(torch.bool)
+        # targets = targets.to(torch.bool)
         print(data.size(), targets.size())
-        #print(net)
+        # print(net)
         net.train()
-        #sprint(net)
+        # sprint(net)
         spk_rec = forward_pass(net, data)
         loss_val = loss_fn(spk_rec, targets)
 
@@ -127,7 +144,8 @@ for epoch in range(num_epochs):
 
         # Store loss history for future plotting
         loss_hist.append(loss_val.item())
-
+        print('{} seconds'.format(time.time() - t0))
+        t0 = time.time()
         print(f"Epoch {epoch}, Iteration {i} \nTrain Loss: {loss_val.item():.2f}")
 
         acc = SF.accuracy_rate(spk_rec, targets)
@@ -137,13 +155,6 @@ for epoch in range(num_epochs):
         # training loop breaks after 50 iterations
         if i == num_iters:
             break
-
-"""
-#######
-RESULTS
-#######
-"""
-# Plot Loss
 fig = plt.figure(facecolor="w")
 plt.plot(acc_hist)
 plt.title("Train Set Accuracy")
@@ -151,13 +162,44 @@ plt.xlabel("Iteration")
 plt.ylabel("Accuracy")
 plt.show()
 
+# with torch.no_grad():
+net.eval()
+for i, (data, targets) in enumerate(testloader, 0):
+    data = data.to(device)
+    targets = targets.to(device)
+    print(data.size(), targets.size())
+    spk_rec = forward_pass(net, data)
+    loss_val = loss_fn(spk_rec, targets)
+
+    # Gradient calculation + weight update
+    optimizer.zero_grad()
+    loss_val.backward()
+    optimizer.step()
+
+    # Store loss history for future plotting
+    test_loss_hist.append(loss_val.item())
+
+    print(f"Iteration {i} \nTrain Loss: {loss_val.item():.2f}")
+
+    acc = SF.accuracy_rate(spk_rec, targets)
+    test_acc_hist.append(acc)
+    print(f"Accuracy: {acc * 100:.2f}%\n")
+
+    # training loop breaks after 50 iterations
+    if i == num_iters:
+        break
+
+"""
+#######
+RESULTS
+#######
+"""
 # Plot Loss
+print(np.mean(test_acc_hist))
+
 fig = plt.figure(facecolor="w")
-plt.plot(loss_hist)
-plt.title("Train Set Loss")
+plt.plot(test_acc_hist)
+plt.title("Test Set Accuracy")
 plt.xlabel("Iteration")
-plt.ylabel("Loss")
+plt.ylabel("Accuracy")
 plt.show()
-
-
-
--- mnist_test.py
+++ mnist_test.py
@@ -1,18 +1,20 @@
+import time
+
 import matplotlib.pyplot as plt
+import numpy as np
+import snntorch as snn
 import tonic
 import tonic.transforms as transforms
 import torch
 import torch.nn as nn
 import torchvision
-from torch.utils.data import DataLoader
-from tonic import DiskCachedDataset
-
-import snntorch as snn
 from snntorch import functional as SF
 from snntorch import surrogate
 from snntorch import utils
+from torch.utils.data import DataLoader
 
-from pytorch_model_summary import summary
+import snntorch.spikeplot as splt
+from IPython.display import HTML
 
 """
 #########################################
@@ -39,13 +41,12 @@ trainset = tonic.datasets.NMNIST(save_to='./data', transform=frame_transform, tr
 testset = tonic.datasets.NMNIST(save_to='./data', transform=frame_transform, train=False)
 
 transform = tonic.transforms.Compose([torch.from_numpy,
-                                      torchvision.transforms.RandomRotation([-10,10])])
+                                      torchvision.transforms.RandomRotation([-10, 10])])
 
-cached_trainset = DiskCachedDataset(trainset, transform=transform, cache_path='./cache/nmnist/train')
+# cached_trainset = DiskCachedDataset(trainset, transform=transform, cache_path='./cache/nmnist/train')
 
 # no augmentations for the testset
-cached_testset = DiskCachedDataset(testset, cache_path='./cache/nmnist/test')
-
+# cached_testset = DiskCachedDataset(testset, cache_path='./cache/nmnist/test')
 
 """
 #####################################################
@@ -59,9 +60,22 @@ TRAINING OUR NETWORK USING FRAMES CREATED FROM EVENTS
 # no augmentations for the testset
 # cached_testset = CachedDataset(testset, cache_path='./cache/nmnist/test')
 
-batch_size = 128
-trainloader = DataLoader(cached_trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
-testloader = DataLoader(cached_testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors)
+batch_size = 32
+trainloader = DataLoader(trainset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=True)
+testloader = DataLoader(testset, batch_size=batch_size, collate_fn=tonic.collation.PadTensors(), shuffle=False)
+
+
+n = 125
+
+# index into a single sample and sum the on/off channels
+a = (trainloader.dataset[n][0][:, 0] + trainloader.dataset[n][0][:, 1])
+
+#  Plot
+fig, ax = plt.subplots()
+anim = splt.animator(a, fig, ax, interval=10)
+HTML(anim.to_html5_video())
+
+anim.save('dvsgesture_animator.mp4', writer = 'ffmpeg', fps=50)
 
 """
 DEFINING OUR NETWORK
@@ -69,7 +83,7 @@ DEFINING OUR NETWORK
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 # network parameters
-num_inputs = 28*28
+num_inputs = 28 * 28
 num_hidden = 128
 num_outputs = 10
 num_steps = 1
@@ -80,7 +94,7 @@ grad = surrogate.fast_sigmoid()
 
 #  Initialize Network
 net = nn.Sequential(nn.Flatten(),
-                    nn.Linear(34*34*2, 10),
+                    nn.Linear(34 * 34 * 2, 10),
                     snn.Leaky(beta=beta, spike_grad=grad, init_hidden=True, output=True)
                     ).to(device)
 
@@ -105,21 +119,22 @@ optimizer = torch.optim.Adam(net.parameters(), lr=2e-2, betas=(0.9, 0.999))
 loss_fn = SF.ce_rate_loss()
 
 num_epochs = 1
-num_iters = 50
+num_iters = 500
 
 loss_hist = []
 acc_hist = []
-
+t0 = time.time()
 # training loop
 for epoch in range(num_epochs):
+
     for i, (data, targets) in enumerate(iter(trainloader)):
         data = data.to(device)
 
         targets = targets.to(device)
         print(data.size(), targets.size())
-        #print(net)
+        # print(net)
         net.train()
-        #sprint(net)
+        # sprint(net)
         spk_rec = forward_pass(net, data)
         loss_val = loss_fn(spk_rec, targets)
 
@@ -150,6 +165,43 @@ RESULTS
 fig = plt.figure(facecolor="w")
 plt.plot(acc_hist)
 plt.title("Train Set Accuracy")
+plt.xlabel("Iteration")
+plt.ylabel("Accuracy")
+plt.show()
+
+test_loss_hist = []
+test_acc_hist = []
+net.eval()
+for i, (data, targets) in enumerate(iter(testloader)):
+    data = data.to(device)
+    targets = targets.to(device)
+    print(data.size(), targets.size())
+    spk_rec = forward_pass(net, data)
+    loss_val = loss_fn(spk_rec, targets)
+
+    # Gradient calculation + weight update
+    optimizer.zero_grad()
+    loss_val.backward()
+    optimizer.step()
+
+    # Store loss history for future plotting
+    test_loss_hist.append(loss_val.item())
+
+    print(f"Iteration {i} \nTrain Loss: {loss_val.item():.2f}")
+
+    acc = SF.accuracy_rate(spk_rec, targets)
+    test_acc_hist.append(acc)
+    print(f"Accuracy: {acc * 100:.2f}%\n")
+
+    # training loop breaks after 50 iterations
+    if i == num_iters:
+        break
+
+print(np.mean(test_acc_hist))
+# Plot Loss
+fig = plt.figure(facecolor="w")
+plt.plot(test_acc_hist)
+plt.title("Test Set Accuracy")
 plt.xlabel("Iteration")
 plt.ylabel("Accuracy")
 plt.show()
