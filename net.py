1358dc82ece37a49169ee1b93e48795cb789c77b
Yara Schattschneider <61124827+yarasc@users.noreply.github.com>
2022-04-28 10:34:09 +0200

    extra layer for FFN

--- net.py
+++ net.py
@@ -22,7 +22,7 @@ class RNNet(nn.Module):
         spk_rec = []
         mem_rec = []
 
-        for step in range(50):
+        for step in range(x.size(0)):
             cur = self.fc1(x)
             spk1, mem1 = self.lif1(cur, mem1)
             cur = self.fc1(x)
@@ -30,7 +30,7 @@ class RNNet(nn.Module):
             spk_rec.append(spk2)
             mem_rec.append(mem2)
 
-        return torch.stack(spk_rec), torch.stack(mem_rec)
+        return torch.stack(spk_rec)
 
 
 class FFNet(nn.Module):
@@ -58,3 +58,32 @@ class FFNet(nn.Module):
             #mem_rec.append(mem)
 
         return torch.stack(spk_rec, dim=0)#, torch.stack(mem_rec, dim=0)
+
+class TwoFFNet(nn.Module):
+    def __init__(self, num_inputs, num_hidden, num_outputs, beta):
+        super().__init__()
+
+        # initialize layers
+        self.fc1 = nn.Linear(num_inputs, num_hidden)
+        self.lif1 = snn.Leaky(beta=beta)
+        self.fc2 = nn.Linear(num_hidden, num_outputs)
+        self.lif2 = snn.Leaky(beta=beta)
+
+    def forward(self, x):
+        # Initialize hidden states and outputs at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+
+        # Record the final layer
+        spk_rec = []
+        mem_rec = []
+
+        for step in range(x.size(0)):
+            cur = self.fc1(x)
+            spk1, mem1 = self.lif1(cur, mem1)
+            cur = self.fc1(x)
+            spk2, mem2 = self.lif1(cur, mem2)
+            spk_rec.append(spk2)
+            mem_rec.append(mem2)
+
+        return torch.stack(spk_rec)
