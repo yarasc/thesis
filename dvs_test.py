8a8306b9fd8fc516218b9d6953748bdef9f658a3
Yara Schattschneider <61124827+yarasc@users.noreply.github.com>
2022-04-29 19:14:46 +0200

    pooled input

--- dvs_test.py
+++ dvs_test.py
@@ -1,5 +1,3 @@
-import time
-
 from snntorch import surrogate
 
 from net import *
@@ -10,7 +8,7 @@ Feedforward NN with LIF
 based on Tutorial 7 SNNTorch
 """
 
-batch_size = 16
+batch_size = 1
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
@@ -21,26 +19,30 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 # num_classes = 10
 
 # for DVS Gesture
-num_inputs = 128 * 128 * 2  #height*channels*width (on-spikes for luminance increasing; off-spikes for luminance decreasing)
-num_hidden = 128 * 128 * 2
+num_inputs = 128 * 128 * 2  # height*channels*width (on-spikes for luminance increasing; off-spikes for luminance decreasing)
+num_hidden = 128 * 16
 num_classes = 11
+#Number of train samples: 1176
+#Number of test samples: 288
 
 # spiking neuron parameters
 beta = 0.9  # neuron decay rate
-grad = surrogate.fast_sigmoid()
+grad = surrogate.fast_sigmoid(slope=25)
 
 # Epochs & Iterations
 num_epochs = 10
-num_train = 50
-num_test = 15 # 15 => maximum amount of steps
+num_train = 500
+num_test = 100  # 15 => maximum amount of steps
 
 trainloader, testloader = createDataloaders(batch_size)
-net = RNNet(num_inputs, num_hidden, num_classes, beta).to(device)
-#net = TwoFFNet(num_inputs, num_hidden, num_classes, beta).to(device)
-#net = FFNet(num_inputs, num_classes, beta).to(device)
+
+# net = RNNet(int(num_inputs/16), num_hidden, num_classes, beta).to(device)
+#net = TwoFFNet(int(num_inputs / 16), num_hidden, num_classes, beta).to(device)
+net = FFNet(int(num_inputs / 16), num_classes, beta).to(device)
+# net = RFFNet(int(num_inputs / 16), num_classes, beta).to(device)
 
 optimizer = torch.optim.Adam(net.parameters(), lr=2e-2, betas=(0.9, 0.999))
-loss_fn = SF.ce_rate_loss()
+loss_fn = SF.ce_count_loss()
 # loss_fn = SF.mse_count_loss(correct_rate=0.8, incorrect_rate=0.2)
 
 
@@ -49,25 +51,26 @@ test_hist = pd.DataFrame(columns=["Epoch", "Iteration", "Accuracy", "Loss"])
 
 # training loop
 t0 = time.time()
-
+pool = nn.MaxPool2d(4)
+flatten = nn.Flatten()
 for epoch in range(num_epochs):
     for i, (data, targets) in enumerate(trainloader, 0):
-        #data = data.to(device)
+        data = data.to(device)
         targets = targets.to(device)
 
         net.train()
-
-        spk_rec = []
         # compute prediction and loss
-        for step in range(data.size(0)):  # data.size(0) = number of time steps
-            #TODO move this into for loop to allocate less memory ?
-            step_data = data[step]
-            step_data = step_data.to(device)
+        # for step in range(data.size(0)):  # data.size(0) = number of time steps
+        # TODO move this into for loop to allocate less memory ?
+        # step_data = data[step]
+        # step_data = data[step].to(device)
+        compact = pool(data.squeeze())
 
-            spk_out = net(step_data.view(batch_size, -1))
-            spk_rec.append(spk_out)
-        spk_rec = torch.stack(spk_rec)
-        spk_rec = spk_rec[:, :, -1, :]
+        compact = torch.unsqueeze(compact, 1)
+        spk_rec = net(flatten(compact))
+
+        spk_rec = spk_rec.unsqueeze(dim=1)
+        result = spk_rec.sum(dim=0)
         loss_val = loss_fn(spk_rec, targets)
 
         # Gradient calculation + weight update
@@ -83,27 +86,29 @@ for epoch in range(num_epochs):
             break
 
     for i, (data, targets) in enumerate(testloader, 0):
-        #data = data.to(device)
+        # data = data.to(device)
         targets = targets.to(device)
         net.eval()
 
         spk_rec = []
-        for step in range(data.size(0)):  # data.size(0) = number of time steps
-            step_data = data[step]
-            step_data = step_data.to(device)
 
-            spk_out = net(data[step].view(batch_size, -1))
-            spk_rec.append(spk_out)
-        spk_rec = torch.stack(spk_rec, dim=0)
-        spk_rec = spk_rec[:, :, -1, :]
-        loss_val = loss_fn(spk_rec, targets)
+        compact = pool(data.squeeze())
 
+        compact = torch.unsqueeze(compact, 1)
+        # print(step_data.shape, compact.shape, step_data.view(batch_size, -1).shape)
+        spk_rec = net(flatten(compact))
+
+        loss_val = loss_fn(spk_rec.unsqueeze(dim=1), targets)
+
+        #accuracy = np.mean((targets == idx).detach().cpu().numpy())
         # Store loss history for future plotting every 0.3 second
-        hist = evaluate(spk_rec, targets, t0, loss_val, epoch, i, train=False)
-        train_hist = pd.concat([train_hist, hist])
+        hist = evaluate(spk_rec.unsqueeze(dim=1), targets, t0, loss_val, epoch, i, train=False)
+        test_hist = pd.concat([test_hist, hist])
         t0 = time.time()
         if i == num_test:
             break
+    for param in net.parameters():
+        print(param.data)
 
 train_hist.to_csv('train.csv')
-train_hist.to_csv('test.csv')
+test_hist.to_csv('test.csv')
--- net.py
+++ net.py
@@ -1,6 +1,8 @@
+import snntorch as snn
 import torch
 import torch.nn as nn
-import snntorch as snn
+from snntorch import surrogate
+from snntorch._neurons.neurons import *
 
 
 class RNNet(nn.Module):
@@ -10,8 +12,8 @@ class RNNet(nn.Module):
         # initialize layers
         self.fc1 = nn.Linear(num_inputs, num_hidden)
         self.lif1 = snn.Leaky(beta=beta)
-        self.fc2 = nn.RNN(num_hidden, num_outputs)
-        self.lif2 = snn.Leaky(beta=beta)
+        self.fc2 = nn.RNN(num_hidden, 11)
+        self.lif2 = snn.Leaky(beta=beta, output=True, init_hidden=True)
 
     def forward(self, x):
         # Initialize hidden states and outputs at t=0
@@ -23,41 +25,43 @@ class RNNet(nn.Module):
         mem_rec = []
 
         for step in range(x.size(0)):
-            cur = self.fc1(x)
+            cur = self.fc1(x[step])
             spk1, mem1 = self.lif1(cur, mem1)
-            cur = self.fc1(x)
-            spk2, mem2 = self.lif1(cur, mem2)
+            cur2 = self.fc2(spk1)
+            spk2, mem2 = self.lif2(cur2, mem2)
             spk_rec.append(spk2)
-            mem_rec.append(mem2)
 
         return torch.stack(spk_rec)
 
 
-class FFNet(nn.Module):
+class RFFNet(nn.Module):
     def __init__(self, num_inputs, num_outputs, beta):
         super().__init__()
 
         # initialize layers
-        #self.flat = nn.Flatten()
-        self.fc1 = nn.Linear(num_inputs, num_outputs)
-        self.lif1 = snn.Leaky(beta=beta)
+        # self.flat = nn.Flatten()
+        grad = surrogate.fast_sigmoid(slope=25)
 
+        self.fc1 = nn.Linear(num_inputs, num_outputs)
+        self.lif1 = snn.RLeaky(beta=beta, spike_grad=grad, V=0.5)
 
     def forward(self, x):
         # Initialize hidden states at t=0
         mem = self.lif1.init_leaky()
 
         # Record the final layer
-        spk_rec = []
+
         mem_rec = []
-
+        spk_rec = []
+        spk, mem = _SpikeTorchConv(False, False, input_=x[0])
         for step in range(x.size(0)):
-            cur = self.fc1(x)
-            spk, mem = self.lif1(cur, mem)
+            cur = self.fc1(x[step])
+            spk, mem = self.lif1(cur, spk, mem)
             spk_rec.append(spk)
-            #mem_rec.append(mem)
+            # mem_rec.append(mem)
 
-        return torch.stack(spk_rec, dim=0)#, torch.stack(mem_rec, dim=0)
+        return torch.stack(spk_rec)  # , torch.stack(mem_rec, dim=0)
+
 
 class TwoFFNet(nn.Module):
     def __init__(self, num_inputs, num_hidden, num_outputs, beta):
@@ -67,7 +71,7 @@ class TwoFFNet(nn.Module):
         self.fc1 = nn.Linear(num_inputs, num_hidden)
         self.lif1 = snn.Leaky(beta=beta)
         self.fc2 = nn.Linear(num_hidden, num_outputs)
-        self.lif2 = snn.Leaky(beta=beta)
+        self.lif2 = snn.Leaky(beta=beta, output=True)
 
     def forward(self, x):
         # Initialize hidden states and outputs at t=0
@@ -79,11 +83,35 @@ class TwoFFNet(nn.Module):
         mem_rec = []
 
         for step in range(x.size(0)):
-            cur = self.fc1(x)
+            cur = self.fc1(x[step])
             spk1, mem1 = self.lif1(cur, mem1)
-            cur = self.fc1(x)
-            spk2, mem2 = self.lif1(cur, mem2)
+            cur2 = self.fc2(spk1)
+            spk2, mem2 = self.lif2(cur2, mem2)
             spk_rec.append(spk2)
-            mem_rec.append(mem2)
 
         return torch.stack(spk_rec)
+
+
+class FFNet(nn.Module):
+    def __init__(self, num_inputs, num_outputs, beta):
+        super().__init__()
+        grad = surrogate.fast_sigmoid(slope=25)
+        # initialize layers
+        # self.flat = nn.Flatten()
+        self.fc1 = nn.Linear(num_inputs, num_outputs)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=grad)
+
+    def forward(self, x):
+        # Initialize hidden states at t=0
+        mem = self.lif1.init_leaky()
+
+        # Record the final layer
+        spk_rec = []
+        mem_rec = []
+        for step in range(x.size(0)):
+            cur = self.fc1(x[step])
+            spk, mem = self.lif1(cur, mem)
+            spk_rec.append(spk)
+            # mem_rec.append(mem)
+
+        return torch.stack(spk_rec)  # , torch.stack(mem_rec, dim=0)
