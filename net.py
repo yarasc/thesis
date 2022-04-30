c0ab0ea7698001f7d4561bc659330f4af8842794
Yara Schattschneider <61124827+yarasc@users.noreply.github.com>
2022-04-30 17:15:41 +0200

    cleaned up, RFF with RLeaky

--- net.py
+++ net.py
@@ -5,35 +5,6 @@ from snntorch import surrogate
 from snntorch._neurons.neurons import *
 
 
-class RNNet(nn.Module):
-    def __init__(self, num_inputs, num_hidden, num_outputs, beta):
-        super().__init__()
-
-        # initialize layers
-        self.fc1 = nn.Linear(num_inputs, num_hidden)
-        self.lif1 = snn.Leaky(beta=beta)
-        self.fc2 = nn.RNN(num_hidden, 11)
-        self.lif2 = snn.Leaky(beta=beta, output=True, init_hidden=True)
-
-    def forward(self, x):
-        # Initialize hidden states and outputs at t=0
-        mem1 = self.lif1.init_leaky()
-        mem2 = self.lif2.init_leaky()
-
-        # Record the final layer
-        spk_rec = []
-        mem_rec = []
-
-        for step in range(x.size(0)):
-            cur = self.fc1(x[step])
-            spk1, mem1 = self.lif1(cur, mem1)
-            cur2 = self.fc2(spk1)
-            spk2, mem2 = self.lif2(cur2, mem2)
-            spk_rec.append(spk2)
-
-        return torch.stack(spk_rec)
-
-
 class RFFNet(nn.Module):
     def __init__(self, num_inputs, num_outputs, beta):
         super().__init__()
@@ -44,16 +15,16 @@ class RFFNet(nn.Module):
 
         self.fc1 = nn.Linear(num_inputs, num_outputs)
         self.lif1 = snn.RLeaky(beta=beta, spike_grad=grad, V=0.5)
+        #V: Recurrent weights to scale output spikes
+
 
     def forward(self, x):
         # Initialize hidden states at t=0
         mem = self.lif1.init_leaky()
 
-        # Record the final layer
-
         mem_rec = []
         spk_rec = []
-        spk, mem = _SpikeTorchConv(False, False, input_=x[0])
+        spk = x[0]
         for step in range(x.size(0)):
             cur = self.fc1(x[step])
             spk, mem = self.lif1(cur, spk, mem)
@@ -97,7 +68,6 @@ class FFNet(nn.Module):
         super().__init__()
         grad = surrogate.fast_sigmoid(slope=25)
         # initialize layers
-        # self.flat = nn.Flatten()
         self.fc1 = nn.Linear(num_inputs, num_outputs)
         self.lif1 = snn.Leaky(beta=beta, spike_grad=grad)
 
