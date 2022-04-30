17c8f35d6f14cd23a2f4ea731bf1454502e44906
Yara Schattschneider <61124827+yarasc@users.noreply.github.com>
2022-04-30 17:14:36 +0200

    mse loss, preprocess for batches
    mse count loss seems to minimise the dead neurons (no spikes fired)

--- dvs_test.py
+++ dvs_test.py
@@ -1,5 +1,3 @@
-from snntorch import surrogate
-
 from net import *
 from utils import *
 
@@ -14,16 +12,23 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 
 # network parameters
 # for NMNIST
+# trainloader, testloader = createMNISTDataloaders(batch_size)
 # num_inputs = 2 * 34 * 34  # width*height*channels (on-spikes for luminance increasing; off-spikes for luminance decreasing)
 # num_hidden = 2 * 34 * 34
 # num_classes = 10
+# kernel = 1
 
 # for DVS Gesture
-num_inputs = 128 * 128 * 2  # height*channels*width (on-spikes for luminance increasing; off-spikes for luminance decreasing)
-num_hidden = 128 * 16
+# Number of train samples: 1176
+# Number of test samples: 288
+trainloader, testloader = createDataloaders(batch_size)
+kernel = 4
+num_inputs = int(128 * 128 * 2 / (kernel*kernel)) # height*channels*width (on-spikes for luminance increasing; off-spikes for luminance decreasing)
+num_hidden = 128 * 128 * 2
 num_classes = 11
-#Number of train samples: 1176
-#Number of test samples: 288
+
+
+
 
 # spiking neuron parameters
 beta = 0.9  # neuron decay rate
@@ -31,19 +36,21 @@ grad = surrogate.fast_sigmoid(slope=25)
 
 # Epochs & Iterations
 num_epochs = 10
-num_train = 500
-num_test = 100  # 15 => maximum amount of steps
+num_train = 50
+num_test = 15  # 15 => maximum amount of steps in DVS Gesture if batchsize 16
 
-trainloader, testloader = createDataloaders(batch_size)
 
-# net = RNNet(int(num_inputs/16), num_hidden, num_classes, beta).to(device)
-#net = TwoFFNet(int(num_inputs / 16), num_hidden, num_classes, beta).to(device)
-net = FFNet(int(num_inputs / 16), num_classes, beta).to(device)
-# net = RFFNet(int(num_inputs / 16), num_classes, beta).to(device)
+# net = FFNet(num_inputs, num_classes, beta).to(device)
+net = RFFNet(num_inputs, num_classes, beta).to(device)
 
 optimizer = torch.optim.Adam(net.parameters(), lr=2e-2, betas=(0.9, 0.999))
-loss_fn = SF.ce_count_loss()
-# loss_fn = SF.mse_count_loss(correct_rate=0.8, incorrect_rate=0.2)
+# Cross Entropy encourages the correct class to fire at all time steps, and aims to suppress incorrect classes from firing
+#loss_fn = SF.ce_rate_loss() # Cross Entropy Spike Rate Loss, applies the Cross Entropy function at every time step
+#loss_fn = SF.ce_count_loss() # Cross Entropy Spike Count Loss, accumulates spikes first & applies Cross Entropy Loss only once
+# loss_fn = SF.ce_max_membrane_loss() # encourages the maximum membrane potential of the correct class to increase, while suppressing the maximum membrane potential of incorrect classes
+
+loss_fn = SF.mse_count_loss(correct_rate=1, incorrect_rate=0.8) #default rate correct=1, incorrect=1, tutorial: 0.8,0.2 -> avoid dead neurones
+#loss_fn = SF.mse_membrane_loss()
 
 
 train_hist = pd.DataFrame(columns=["Epoch", "Iteration", "Accuracy", "Loss"])
@@ -51,26 +58,16 @@ test_hist = pd.DataFrame(columns=["Epoch", "Iteration", "Accuracy", "Loss"])
 
 # training loop
 t0 = time.time()
-pool = nn.MaxPool2d(4)
-flatten = nn.Flatten()
+flatten = nn.Flatten(2, 4)
+pool = nn.MaxPool3d(kernel_size=(1,int(kernel),int(kernel)))
+
 for epoch in range(num_epochs):
     for i, (data, targets) in enumerate(trainloader, 0):
         data = data.to(device)
         targets = targets.to(device)
 
         net.train()
-        # compute prediction and loss
-        # for step in range(data.size(0)):  # data.size(0) = number of time steps
-        # TODO move this into for loop to allocate less memory ?
-        # step_data = data[step]
-        # step_data = data[step].to(device)
-        compact = pool(data.squeeze())
-
-        compact = torch.unsqueeze(compact, 1)
-        spk_rec = net(flatten(compact))
-
-        spk_rec = spk_rec.unsqueeze(dim=1)
-        result = spk_rec.sum(dim=0)
+        spk_rec = net(flatten(pool(data)))
         loss_val = loss_fn(spk_rec, targets)
 
         # Gradient calculation + weight update
@@ -81,7 +78,8 @@ for epoch in range(num_epochs):
         hist = evaluate(spk_rec, targets, t0, loss_val, epoch, i, train=True)
         train_hist = pd.concat([train_hist, hist])
         t0 = time.time()
-        # training loop breaks after 50 iterations
+
+        # training loop breaks after x iterations
         if i == num_train:
             break
 
@@ -90,23 +88,19 @@ for epoch in range(num_epochs):
         targets = targets.to(device)
         net.eval()
 
-        spk_rec = []
-
-        compact = pool(data.squeeze())
-
-        compact = torch.unsqueeze(compact, 1)
         # print(step_data.shape, compact.shape, step_data.view(batch_size, -1).shape)
-        spk_rec = net(flatten(compact))
+        spk_rec = net(flatten(pool(data)))
 
-        loss_val = loss_fn(spk_rec.unsqueeze(dim=1), targets)
+        loss_val = loss_fn(spk_rec, targets)
 
-        #accuracy = np.mean((targets == idx).detach().cpu().numpy())
-        # Store loss history for future plotting every 0.3 second
-        hist = evaluate(spk_rec.unsqueeze(dim=1), targets, t0, loss_val, epoch, i, train=False)
+        # Store loss history for future plotting
+        hist = evaluate(spk_rec, targets, t0, loss_val, epoch, i, train=False)
         test_hist = pd.concat([test_hist, hist])
         t0 = time.time()
         if i == num_test:
             break
+
+    #print parameter weights after each epoch.
     for param in net.parameters():
         print(param.data)
 
